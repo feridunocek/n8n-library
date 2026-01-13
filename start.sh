24fa5acf6ef5ff094a4cccec4b034e81d62e5665
Feridun <feridun@Feriduns-MacBook-Pro.local>
2026-01-13 19:03:34 +0100

    Force use of Prisma 5 in start script

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
 # Run Prisma DB Push to ensure schema is secure
 echo "Regenerating Prisma Client..."
-npx prisma generate
+npx prisma@5.22.0 generate
 echo "Pushing database schema..."
-npx prisma db push --accept-data-loss
+npx prisma@5.22.0 db push --accept-data-loss
 
 # Check if migrations ran successfully (optional but good context)
 if [ $? -eq 0 ]; then
